429c2cf39cde470b07f9245d5008ba930543f66c
Victor Zerefos <victor.zerefos@gmail.com>
2024-12-29 08:48:26 -0300

    Initial Commit

---
+++ CustomNavBar/CustomNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarApp.swift
+//  CustomNavBar
+//
+//  Created by Victor Zerefos on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
